b267c672ab4e45b49b8bbee48b4ae67910e28517
the@coder <ahmadraza001912@gmail.com>
2025-03-22 13:50:04 +0530

    added the host

--- donate/settings.py
+++ donate/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='your_default_secret_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rankuptechnology.pythonanywhere.com',"pay.angelsfoundationindia.org"]
 
 
 # Application definition
