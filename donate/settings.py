4c4c4629c1ccc8f5ae19c5e0d25b53e2ee3f05f5
the@coder <ahmadraza001912@gmail.com>
2025-03-27 16:31:39 +0530

    added the rest framework

--- donate/settings.py
+++ donate/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #restframeworks
+    "rest_framework",
+
     # Custom apps
     'backend_apps.central',
     'backend_apps.donate_once',
