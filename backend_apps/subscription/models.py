a0f32d7db6736010665eaf2e741cb5b2df748140
the@coder <ahmadraza001912@gmail.com>
2025-04-02 17:35:42 +0530

    removed the site from subscription

--- backend_apps/subscription/models.py
+++ backend_apps/subscription/models.py
@@ -21,7 +21,7 @@ class Subscription(models.Model):
         ("y.com", "y.com"),
     ]
 
-    site = models.CharField(max_length=10, choices=SITE_CHOICES, default="x.com")
+    # site = models.CharField(max_length=10, choices=SITE_CHOICES, default="x.com")
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     mobile = models.CharField(max_length=255)  # Encrypted data
     name = models.CharField(max_length=255)  # Encrypted data
